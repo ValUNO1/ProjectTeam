1df6b9bcf7d255dde945acda998ab6f67181792c
Dorcas97 <98547266+Dorcas97@users.noreply.github.com>
2022-03-23 20:12:05 -0500

    commit 3/23

--- mfl_bookstore/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from .models import *
-from .forms import *
-
-def upload_book(request):
-
-	book = Book.objects.filter()
-	return render(request, './upload_book.html', {'books': book})
