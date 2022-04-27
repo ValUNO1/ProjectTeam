2fdbe0b5eea63e75947453235a05178439f9962e
Dorcas97 <98547266+Dorcas97@users.noreply.github.com>
2022-04-27 16:33:08 -0500

    commit 4/27

--- mfl_bookstore/settings.py
+++ mfl_bookstore/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-=hkbgmw02f!*-*n4(3w#8@_$*(*s_(ua_ipf6oqi&d@$v#hq)b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
